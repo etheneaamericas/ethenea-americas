ae4531cb8a7bac40760c9a483f0a3033bd2f2a36
ETHENEA(ETHENEA Americas LLC) <lamessuddin385@gmail.com>
2026-04-09 03:17:28 -0400

    Create ethenea.py

---
+++ ethenea.py
@@ -0,0 +1,9 @@
+class ETHENEA:
+    def __init__(self):
+        self.company = "ETHENEA Americas LLC"
+
+    def info(self):
+        return "AI + Crypto Infrastructure Platform"
+
+ethenea = ETHENEA()
+print(ethenea.info())
